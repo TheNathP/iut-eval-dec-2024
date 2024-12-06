a6cc75429420bad7df7880ca46dd0d85697dbce4
Nathan <nathanpiet2004@gmail.com>
2024-12-06 14:06:53 +0100

    done

--- src/js/components/Recipes.js
+++ src/js/components/Recipes.js
@@ -3,9 +3,44 @@ import allRecipes from "../../data/recipes.json";
 export default function Recipes() {
     return {
         recipes: [],
+        selectedDifficulty: "",
+        selectedRecipe: null,
+        currentStep: 0,
 
         init() {
             this.recipes = allRecipes.recipes;
+        },
+
+        get filteredRecipes() {
+            // Si aucune difficulté n'est sélectionnée, retournez toutes les recettes
+            if (!this.selectedDifficulty) {
+                return this.recipes;
+            }
+            // Filtrer les recettes par difficulté
+            return this.recipes.filter(
+                (recipe) => recipe.difficulty === this.selectedDifficulty
+            );
+        },
+
+        viewRecipe(recipe) {
+            this.selectedRecipe = recipe;
+            this.currentStep = 0;
+        },
+
+        closeRecipe() {
+            this.selectedRecipe = null;
+        },
+
+        nextStep() {
+            if (this.currentStep < this.selectedRecipe.instructions.length - 1) {
+                this.currentStep++;
+            }
+        },
+
+        prevStep() {
+            if (this.currentStep > 0) {
+                this.currentStep--;
+            }
         }
-    }
+    };
 }
